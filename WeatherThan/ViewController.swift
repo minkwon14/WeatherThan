443b077ef27922d35e26c991e4c3245c2810cc47
minkwon14 <min.kwon14@gmail.com>
2017-11-21 21:39:55 -0500

    Initial Commit

---
+++ WeatherThan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherThan
+//
+//  Created by Minseo Kwon on 2017-11-21.
+//  Copyright © 2017 MinKwon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
